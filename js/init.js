f09beca5d03545b09e443582c5a3945618e9feda
bendroid <collins.drums@gmail.com>
2015-08-07 00:43:32 -0400

    update

--- js/init.js
+++ js/init.js
@@ -1,34 +1,20 @@
-(function($){
-  $(function(){
+'use strict';
+(function ($) {
+    $(function () {
 
-    // Initialize collapse button
-    $('.button-collapse').sideNav();
+        // Initialize collapse button
+        $('.button-collapse').sideNav();
 
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    $('.collapsible').collapsible();
+        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+        $('.collapsible').collapsible();
 
 
-    $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
+        $('.dropdown-button').dropdown({
+            constrain_width: true, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: true // Displays dropdown below the button
+        });
 
-    $('.dropdown-button').dropdown({
-      constrain_width: true, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true // Displays dropdown below the button
-    }
-  );
-
-	$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
-  
-  
-  $(document).ready(function(){
-    $('.tooltipped').tooltip({delay: 50});
-  });
-
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
